1b653e6f2a4c9613f021e9a13c58bba68d3519f0
khangttse182098 <khangttse182098@fpt.edu.vn>
2025-04-10 12:06:33 +0700

    fix bufferline background

--- .config/nvim/lua/cisco/keymap.lua
+++ .config/nvim/lua/cisco/keymap.lua
@@ -1,3 +1,5 @@
+local opts = { noremap = true, silent = true }
+
 -- Set leader to Space
 vim.g.mapleader = " "
 
@@ -6,41 +8,48 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 -- Copy to system clipboard
-vim.keymap.set('n', '<leader>y', '"+y', { noremap = true, silent = true })
-vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>y", '"+y', opts)
+vim.keymap.set("v", "<leader>y", '"+y', opts)
 
 --paste without losing buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Center screen when ctrl+d and ctrl+u
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+--split command
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", opts)
+vim.keymap.set("n", "<leader>s", ":split<CR>", opts)
+vim.keymap.set("n", "<leader>l", "<C-w>l", opts)
+vim.keymap.set("n", "<leader>h", "<C-w>h", opts)
+vim.keymap.set("n", "<leader>j", "<C-w>j", opts)
+vim.keymap.set("n", "<leader>k", "<C-w>k", opts)
+
 -- Save with ctrl + s and format
-vim.keymap.set({ 'n', 'i' }, '<C-s>', '<Esc><cmd>w<CR><cmd>lua vim.lsp.buf.format()<CR>',
-  { noremap = true, silent = true })
+vim.keymap.set({ "n", "i" }, "<C-s>", "<Esc><cmd>w<CR><cmd>lua vim.lsp.buf.format()<CR>", opts)
 
 --show hover
-vim.keymap.set('n', '<leader>k', function()
-  local float_opts = {
-    focusable = true,
-    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-    border = "rounded",
-    source = "always",
-    prefix = " ",
-    scope = "cursor",
-  }
+vim.keymap.set("n", "<leader>sk", function()
+	local float_opts = {
+		focusable = true,
+		close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+		border = "rounded",
+		source = "always",
+		prefix = " ",
+		scope = "cursor",
+	}
 
-  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
-  if diagnostics and #diagnostics > 0 then
-    vim.diagnostic.open_float(nil, float_opts)
-  else
-    -- Try to use LSP hover with scroll support
-    vim.lsp.buf.hover()
-  end
+	local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+	if diagnostics and #diagnostics > 0 then
+		vim.diagnostic.open_float(nil, float_opts)
+	else
+		-- Try to use LSP hover with scroll support
+		vim.lsp.buf.hover()
+	end
 end, { desc = "Show diagnostics or LSP hover info" })
 
 --rename
-vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = "LSP Rename" })
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "LSP Rename" })
--- .config/nvim/lua/cisco/plugins/bufferline.lua
+++ .config/nvim/lua/cisco/plugins/bufferline.lua
@@ -1,48 +1,53 @@
 return {
-  {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      require("bufferline").setup({
-        options = {
-          diagnostics = "nvim_lsp", -- Enable LSP diagnostics
-          diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icons = {
-              error = "",
-              warning = "",
-              info = "",
-              hint = "󰌵",
-            }
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				highlights = {
+					fill = {
+						bg = "#303030",
+					},
+				},
+				options = {
+					diagnostics = "nvim_lsp", -- Enable LSP diagnostics
+					diagnostics_indicator = function(count, level, diagnostics_dict, context)
+						local icons = {
+							error = "",
+							warning = "",
+							info = "",
+							hint = "󰌵",
+						}
 
-            if diagnostics_dict.error then
-              return " " .. icons.error .. " " .. diagnostics_dict.error
-            elseif diagnostics_dict.warning then
-              return " " .. icons.warning .. " " .. diagnostics_dict.warning
-            elseif diagnostics_dict.info then
-              return " " .. icons.info .. " " .. diagnostics_dict.info
-            elseif diagnostics_dict.hint then
-              return " " .. icons.hint .. " " .. diagnostics_dict.hint
-            end
+						if diagnostics_dict.error then
+							return " " .. icons.error .. " " .. diagnostics_dict.error
+						elseif diagnostics_dict.warning then
+							return " " .. icons.warning .. " " .. diagnostics_dict.warning
+						elseif diagnostics_dict.info then
+							return " " .. icons.info .. " " .. diagnostics_dict.info
+						elseif diagnostics_dict.hint then
+							return " " .. icons.hint .. " " .. diagnostics_dict.hint
+						end
 
-            return ""
-          end
-        },
-      })
+						return ""
+					end,
+				},
+			})
 
-      local keymap = vim.keymap.set
-      local opts = { desc = "Move buffer" }
+			local keymap = vim.keymap.set
+			local opts = { desc = "Move buffer" }
 
-      -- Move between buffers
-      keymap("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-      keymap("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+			-- Move between buffers
+			keymap("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+			keymap("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
 
-      -- Move buffers left and right
-      keymap("n", "<leader><S-h>", "<cmd>BufferLineMovePrev<CR>", opts)
-      keymap("n", "<leader><S-l>", "<cmd>BufferLineMoveNext<CR>", opts)
+			-- Move buffers left and right
+			keymap("n", "<leader><S-h>", "<cmd>BufferLineMovePrev<CR>", opts)
+			keymap("n", "<leader><S-l>", "<cmd>BufferLineMoveNext<CR>", opts)
 
-      -- delete a buffer
-      keymap("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close buffer" })
-    end
-  }
+			-- delete a buffer
+			keymap("n", "<leader>q", "<cmd>bd<CR>", { desc = "Close buffer" })
+		end,
+	},
 }
